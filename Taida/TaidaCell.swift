c8dc28dad64fe65c7e903937fa276f3c2dacd4a8
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-02-16 23:58:37 +0900

    セル内のラベル位置をわかりやすい位置に調整

--- Taida/TaidaCell.swift
+++ Taida/TaidaCell.swift
@@ -14,7 +14,7 @@ class TaidaCell: UITableViewCell {
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        titleLabel = UILabel(frame: CGRect(x: 10, y: 2, width: 300, height: 15));
+        titleLabel = UILabel(frame: CGRect(x: 30, y: 20, width: 300, height: 15));
         titleLabel.text = "oh";
         titleLabel.font = UIFont.systemFont(ofSize: 12)
         self.addSubview(titleLabel);
